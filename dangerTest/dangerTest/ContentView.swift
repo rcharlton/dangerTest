e1a15f3586277050ef60a380771bffed70506b38
Robin Charlton <mail@robincharlton.me>
2024-07-04 14:00:34 +0100

    Initial work

---
+++ dangerTest/dangerTest/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  dangerTest
+//
+//  Created by Robin Charlton on 04/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("DANGER TEST")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dangerTest/dangerTest/dangerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  dangerTestApp.swift
+//  dangerTest
+//
+//  Created by Robin Charlton on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct dangerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
